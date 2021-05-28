1869c4330d53664b56ed93bd42b30505866d10bc
Thomas van der Berg <ik@thomasvanderberg.nl>
2021-05-28 23:35:00 +0200

    First step: removed unneeded stuff, added new custom texts, updated module file

--- go.mod
+++ go.mod
@@ -1,10 +1,8 @@
-module github.com/hackclub/jobs
+module github.com/tmsbrg/maoxianmud
 
-go 1.15
+go 1.13.3
 
 require (
-	github.com/ahmetb/go-cursor v0.0.0-20131010032410-8136607ea412
-	github.com/charmbracelet/glamour v0.2.0
 	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392
 	golang.org/x/sys v0.0.0-20201202213521-69691e467435 // indirect
 	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
--- main.go
+++ main.go
@@ -1,256 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net"
-	"net/http"
-	"net/url"
 	"os"
 	"strings"
 	"text/tabwriter"
-	"time"
 
-	"github.com/ahmetb/go-cursor"
-	"github.com/charmbracelet/glamour"
 	"golang.org/x/crypto/ssh"
 	terminal "golang.org/x/term"
 )
-
-// optimize for terminals with 72 char width
-//
-// i haven't figured out how to get the terminal width from the ssh session
-//
-// for the sake of time, i'm hardcoding it.
-const globalTerminalWidth = 72
-
-func typewrite(w io.Writer, speed time.Duration, content string) {
-	chars := strings.Split(content, "")
-
-	for _, c := range chars {
-		fmt.Fprint(w, c)
-		time.Sleep(speed)
-	}
-}
-
-func typewriteLines(w io.Writer, speed time.Duration, lines []string) {
-	for _, line := range lines {
-		typewrite(w, speed, line)
-	}
-}
-
-type gistCache struct {
-	Expiration time.Time
-	Content    string
-	Rendered   string
-}
-
-type GistService struct {
-	files       [][]string
-	cachedGists map[string]gistCache
-}
-
-func NewGistService(files [][]string) GistService {
-	return GistService{
-		files:       files,
-		cachedGists: map[string]gistCache{},
-	}
-}
-
-func (g GistService) FileNames() []string {
-	fileNames := make([]string, g.Count())
-
-	for i, f := range g.files {
-		fileNames[i] = f[0]
-	}
-
-	return fileNames
-}
-
-func (g GistService) Count() int {
-	return len(g.files)
-}
-
-// returns URL if file exists, empty string if not
-func (g GistService) FileURL(fileName string) string {
-	var url string
-
-	for _, f := range g.files {
-		if fileName == f[0] {
-			url = f[1]
-		}
-	}
-
-	return url
-}
-
-func (g GistService) FileExists(fileName string) bool {
-	return g.FileURL(fileName) != ""
-}
-
-type GistServiceFileType int
-
-const (
-	GistServiceFileTypeGist GistServiceFileType = iota
-	GistServiceFileTypeRepoFile
-)
-
-func (g GistService) urlType(fileURL string) (GistServiceFileType, error) {
-	u, err := url.Parse(fileURL)
-	if err != nil {
-		return -1, err
-	}
-
-	if strings.HasPrefix(u.Host, "gist") {
-		return GistServiceFileTypeGist, nil
-	}
-
-	if strings.Contains(u.Path, "/blob/") {
-		return GistServiceFileTypeRepoFile, nil
-	}
-
-	return -1, errors.New("GistServiceFileType of fileURL not recognized")
-}
-
-func (g GistService) fetchRemoteGistContents(gistURL string) (string, error) {
-	fileType, err := g.urlType(gistURL)
-	if err != nil {
-		return "", err
-	}
-
-	var rawGistURL string
-	switch fileType {
-	case GistServiceFileTypeGist:
-		rawGistURL = gistURL + "/raw"
-	case GistServiceFileTypeRepoFile:
-		u, err := url.Parse(gistURL)
-		if err != nil {
-			return "", err
-		}
-
-		u.Path = strings.Replace(u.Path, "blob/", "", 1)
-
-		u.Host = "raw.githubusercontent.com"
-
-		rawGistURL = u.String()
-	default:
-		return "", errors.New("GistServiceFileType case not handled")
-	}
-
-	fmt.Println(rawGistURL)
-
-	resp, err := http.Get(rawGistURL)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
-}
-
-func (g GistService) FileContents(fileName string) (string, error) {
-	gistURL := g.FileURL(fileName)
-	if gistURL == "" {
-		return "", errors.New("file " + fileName + " does not exist")
-	}
-
-	var cachedGist gistCache
-	if cached, exists := g.cachedGists[fileName]; exists {
-		cachedGist = cached
-	}
-
-	if time.Now().After(cachedGist.Expiration) {
-		content, err := g.fetchRemoteGistContents(gistURL)
-		if err != nil {
-			return "", fmt.Errorf("error fetching remote gist: %v", err)
-		}
-
-		cachedGist.Content = content
-		cachedGist.Expiration = time.Now().Add(5 * time.Minute)
-	}
-
-	g.cachedGists[fileName] = cachedGist
-
-	return cachedGist.Content, nil
-}
-
-func (g GistService) FileRendered(fileName string, darkOrLight string) (string, error) {
-	var cachedGist gistCache
-	if cached, exists := g.cachedGists[fileName]; exists {
-		cachedGist = cached
-	}
-
-	if darkOrLight != "light" && darkOrLight != "dark" && darkOrLight != "" {
-		return "", errors.New("invalid style")
-	}
-
-	if darkOrLight == "" {
-		darkOrLight = "dark"
-	}
-
-	// if possible, just return the prerendered stuff we have
-	if time.Now().Before(cachedGist.Expiration) && cachedGist.Rendered != "" {
-		return cachedGist.Rendered, nil
-	}
-
-	// else, do the whole shebang...
-
-	raw, err := g.FileContents(fileName)
-	if err != nil {
-		return "", err
-	}
-
-	r, err := glamour.NewTermRenderer(
-		glamour.WithStandardStyle(darkOrLight),
-		glamour.WithWordWrap(int(globalTerminalWidth-3)), // 72 default width, (-3 for space for line numbers)
-		glamour.WithBaseURL(g.FileURL(fileName)),
-	)
-	if err != nil {
-		return "", err
-	}
-
-	rendered, err := r.Render(raw)
-	if err != nil {
-		return "", err
-	}
-
-	// custom formatting changes
-
-	var content string
-	lines := strings.Split(string(rendered), "\n")
-
-	for i, l := range lines {
-		// remove first and last two lines (which are blank)
-		if i == 0 || i >= len(lines)-2 {
-			continue
-		}
-
-		// add line numbers (and left pad them)
-		content += fmt.Sprintf("%2v.", i) + l
-
-		// add new lines where needed
-		if i+1 < len(lines) {
-			content += "\n"
-		}
-	}
-
-	// change escaped \- to just - (for the signature at the end of the JDs)
-	content = strings.ReplaceAll(content, `\-`, "-")
-
-	cachedGist.Rendered = content
-
-	g.cachedGists[fileName] = cachedGist
-
-	return cachedGist.Rendered, nil
-}
 
 func main() {
 	var sshPort string
@@ -261,16 +22,6 @@ func main() {
 	} else {
 		sshPort = ":" + envSshPort
 	}
-
-	files := [][]string{
-		[]string{"README.md", "https://gist.github.com/zachlatta/3a5d780da6a3c964677a4f1c4c751f5c"},
-                []string{"clubs_lead.md", "https://gist.github.com/zachlatta/ef83904bfcfddc04bc823355e5bcd280"},
-                []string{"bank_ops_associate.md", "https://github.com/hackclub/v3/blob/main/components/jobs/bank-ops-associate/jd.mdx"},
-                []string{"hired_bank_ops_lead.md", "https://github.com/hackclub/v3/blob/main/components/jobs/bank-ops-lead/jd.mdx"},
-		[]string{"hired_game_designer.md", "https://gist.github.com/zachlatta/a00579cabbd94c98561377eaf369e9a6"},
-	}
-
-	gists := NewGistService(files)
 
 	config := &ssh.ServerConfig{
 		NoClientAuth: true,
@@ -341,91 +92,37 @@ func main() {
 				go func() {
 					defer channel.Close()
 
-					connecting := []string{
-						"...connecting...\r",
-						"...c..o..n..n..e..c..t..i..n..g...\r",
-					}
+					welcome_msg := "Entering the world of Maoxian...\n\n\r"
 
-					connectingSpeed := 100
+					fmt.Fprint(channel, welcome_msg)
 
-					for _, l := range connecting {
-						for _, c := range strings.Split(l, "") {
-							fmt.Fprint(channel, c)
-							time.Sleep(time.Duration(connectingSpeed) * time.Millisecond)
-						}
+					//connected := []string{
+					//	"\r..........................................................\n\r",
+					//	"\n\r",
+					//	"    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ~*~ CONNECTED! ~*~ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)\n\r",
+					//	"\n\r",
+					//	"..........................................................\n\r",
+					//	"\n\r",
+					//	"WELCOME TO THE HACK CLUB JOBS TERMINAL. PLEASE TYPE help TO BEGIN.\n\r",
+					//	"\n\r",
+					//}
 
-						connectingSpeed += 50
-					}
+					//typewriteLines(channel, 25*time.Millisecond, connected)
 
-					connected := []string{
-						"\r..........................................................\n\r",
-						"\n\r",
-						"    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ~*~ CONNECTED! ~*~ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)\n\r",
-						"\n\r",
-						"..........................................................\n\r",
-						"\n\r",
-						"WELCOME TO THE HACK CLUB JOBS TERMINAL. PLEASE TYPE help TO BEGIN.\n\r",
-						"\n\r",
-					}
-
-					typewriteLines(channel, 25*time.Millisecond, connected)
-
-					term := terminal.NewTerminal(channel, `\(•◡•)/ ~> $ `)
-
-					term.AutoCompleteCallback = func(line string, pos int, key rune) (newLine string, newPos int, ok bool) {
-						// only autocomplete when they hit tab
-						if key != '\t' {
-							return newLine, newPos, ok
-						}
-
-						lineParts := strings.Split(line, " ")
-
-						// only autocomplete if they're typing a file into cat
-						if lineParts[0] != "cat" {
-							return newLine, newPos, ok
-						}
-
-						var givenFile string
-						if len(lineParts) > 1 {
-							givenFile = lineParts[1]
-						}
-
-						files := gists.FileNames()
-						fileMatches := []string{}
-
-						for _, fileName := range files {
-							if strings.HasPrefix(fileName, givenFile) {
-								fileMatches = append(fileMatches, fileName)
-							}
-						}
-
-						if len(fileMatches) > 1 {
-							fmt.Fprintln(term, strings.Join(fileMatches, "\t")+"\n")
-						} else if len(fileMatches) == 1 {
-							newLine = strings.Join([]string{"cat", fileMatches[0]}, " ")
-							newPos = len(newLine)
-							ok = true
-						}
-
-						return newLine, newPos, ok
-					}
+					term := terminal.NewTerminal(channel, `(ง˙o˙)ว ~> $ `)
 
 					for {
 						cmds := map[string]func([]string){
 							"help": func(args []string) {
-								fmt.Fprintln(term, `HACK CLUB JOBS TERMINAL, version 1.0.0-release (x86_64).
-
-These shell commands are defined internally. Type `+"`help`"+` to see this
-list.
-`)
+								fmt.Fprintln(term, "Maoxian MUD basic commands\n")
 
 								// use tabwriter to neatly format command help
 								helpWriter := tabwriter.NewWriter(term, 8, 8, 0, '\t', 0)
 
 								commands := [][]string{
-									[]string{"ls", "list contents of current directory"},
-									[]string{"cat [file] [dark or light]", "display contents of current file"},
-									[]string{"exit", "exit the terminal"},
+									[]string{"look", "Show a description of the current room and its contents"},
+									[]string{"move [direction]", "Move to another room. Example: `move n` will go north."},
+									[]string{"exit", "leave the land of Maoxian and return to your boring terminal."},
 								}
 
 								for _, command := range commands {
@@ -433,130 +130,10 @@ list.
 								}
 								helpWriter.Flush()
 
-								fmt.Fprintln(term, "\npsst! try running 'ls' to get started")
-							},
-							"ls": func(args []string) {
-								files := gists.FileNames()
-
-								fmt.Fprintln(term, "you dust off the shelves and find the following files laying about...\n\r")
-
-								fmt.Fprintln(term, strings.Join(files, "\t"))
-							},
-							"cat": func(args []string) {
-								if len(args) == 0 {
-									fmt.Fprintln(term, "meow! please pass me a file! i can't do anything without one!")
-									return
-								}
-
-								argFile := args[0]
-								var darkOrLight string
-
-								if len(args) > 1 {
-									darkOrLight = args[1]
-								}
-
-								if !gists.FileExists(argFile) {
-									fmt.Fprintln(term, "meow! i can't find the file", argFile)
-									return
-								}
-
-								meowText := "  m e e o o o w !  "
-								typewrite(term, 100*time.Millisecond, meowText)
-
-								content, err := gists.FileRendered(argFile, darkOrLight)
-								if err != nil {
-									fmt.Println(err)
-									fmt.Fprintln(term, "meow... i am having trouble accessing my brain (file retrieval error)")
-									return
-								}
-
-								// clear the meow
-								fmt.Fprint(term, "\r"+strings.Repeat(" ", len(meowText))+"\r")
-
-								contentLines := strings.Split(content, "\n")
-
-								linesToShow := 14
-
-								var exitMsg string
-
-								if darkOrLight == "" || darkOrLight == "dark" {
-									exitMsg += " ~ psst. you can switch to light mode with `cat [file] light` ~"
-								} else {
-									exitMsg += " ~ psst. you can switch to dark mode with `cat [file] dark` ~"
-								}
-
-								exitMsg += "\r\n\n easier to read this file online? " + gists.FileURL(argFile) + " ~(˘▾˘~)"
-
-								// if we don't need to page, print and exit
-								if len(contentLines) <= linesToShow {
-									fmt.Fprintln(term, content)
-									fmt.Fprintln(term, exitMsg)
-									return
-								}
-
-								// page!
-								input := make(chan string, 1)
-								finishedPrinting := false
-
-								go func() {
-									fmt.Println("ATTEMPTING TO PAGE")
-									totalLines := len(contentLines)
-									currentLine := 0
-
-									// print the first n lines
-									fmt.Fprintln(term, strings.Join(contentLines[currentLine:linesToShow], "\n"))
-
-									currentLine += linesToShow
-
-									for range input {
-										nextCurrentLine := currentLine + linesToShow
-										if nextCurrentLine > totalLines {
-											nextCurrentLine = totalLines
-										}
-
-										fmt.Fprint(term, cursor.MoveUp(1))
-										fmt.Fprintln(term, strings.Join(contentLines[currentLine:nextCurrentLine], "\n"))
-
-										currentLine = nextCurrentLine
-
-										if currentLine >= totalLines {
-											finishedPrinting = true
-											break
-										}
-									}
-								}()
-
-								for !finishedPrinting {
-									line, err := term.ReadPassword("     ~(press enter to print more...)~")
-									if err != nil {
-										break
-									}
-
-									input <- line
-								}
-
-								fmt.Fprint(term, cursor.MoveUp(1))
-
-								fmt.Fprintln(term, exitMsg)
-							},
-							"pwd": func(args []string) {
-								typewrite(term, 75*time.Millisecond, "you look up, you look down, you look all around. you are completely and utterly lost.\n\r")
-							},
-							"cd": func(args []string) {
-								typewrite(term, 75*time.Millisecond, "what even IS a directory? this is the HACK CLUB JOBS TERMINAL. there are only jobs here.\r\n")
-							},
-							"whoami": func(args []string) {
-								typewrite(term, 75*time.Millisecond, "who ARE you? why are we here? what IS this all about?\r\n")
+								fmt.Fprintln(term, "\npsst! try running 'look' to get started")
 							},
 							"exit": func(args []string) {
-								goodbye := []string{
-									"JOBS TERMINAL OUT. SEE YOU LATER!\r\n",
-									"\nCODE AT https://github.com/hackclub/jobs\r\n",
-									"\nWANT TO TRY SOMETHING FUN? RUN $ ssh sshtron.zachlatta.com\r\n",
-									"\n(~˘▾˘)~\n\n",
-								}
-
-								typewriteLines(term, 25*time.Millisecond, goodbye)
+								fmt.Fprintln(term, "Leaving the land of Maoxian...\r\n")
 
 								channel.Close()
 							},
@@ -581,9 +158,7 @@ list.
 							fmt.Fprintln(term, "")
 						} else if inputCmd != "" {
 							fmt.Fprintln(term, "")
-							fmt.Fprintln(term, inputCmd, `is not a known command.
-
-p.s. this is a custom SSH server, with a custom shell, written in Go. open source at https://github.com/hackclub/jobs!`)
+							fmt.Fprintln(term, inputCmd, "is not a known command.")
 							fmt.Fprintln(term, "")
 						}
 					}
